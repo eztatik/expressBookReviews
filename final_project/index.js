9ea3f3e8d66daea2d1ee81304212380fb6d8757e
eztatik <jromero29knights@gmail.com>
2024-05-02 17:09:38 -0400

    testing js changes

--- final_project/index.js
+++ final_project/index.js
@@ -12,7 +12,9 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+
 });
+//notes to test git
  
 const PORT =5000;
 
